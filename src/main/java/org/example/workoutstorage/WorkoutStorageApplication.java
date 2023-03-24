8424be29ac28e08b161f18538f6c392157f09c24
Evgeni <Kremeni@gmail.com>
2023-03-24 18:09:25 +0300

    init: Create base project

---
+++ src/main/java/org/example/workoutstorage/WorkoutStorageApplication.java
@@ -0,0 +1,13 @@
+package org.example.workoutstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutStorageApplication.class, args);
+    }
+
+}
